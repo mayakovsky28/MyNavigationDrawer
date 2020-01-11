cf145dd3c78be6354d73f2e58e772aa03f5eaf9a
Konstantin <lewiswilliamgreaves@gmail.com>
2020-01-11 08:07:29 +0000

    Added fragments for each section.

--- app/src/main/java/com/lewisgreaves/mynavigationdrawer/MainActivity.java
+++ app/src/main/java/com/lewisgreaves/mynavigationdrawer/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -16,6 +17,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+
+    private Fragment fragmentNews;
+    private Fragment fragmentProfile;
+    private Fragment fragmentSettings;
+
+    private static final int FRAGMENT_NEWS = 0;
+    private static final int FRAGMENT_PROFILE = 1;
+    private static final int FRAGMENT_SETTINGS = 2;
 
 
     @Override
@@ -42,10 +51,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
         switch(id) {
             case R.id.activity_main_drawer_news:
+                this.showFragment(FRAGMENT_NEWS);
                 break;
             case R.id.activity_main_drawer_profile:
+                this.showFragment(FRAGMENT_PROFILE);
                 break;
             case R.id.activity_main_drawer_settings:
+                this.showFragment(FRAGMENT_SETTINGS);
                 break;
             default:
                 break;
@@ -55,6 +67,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         this.drawerLayout.closeDrawer(GravityCompat.START);
 
         return true;
+    }
+
+    private void showFragment (int fragmentIdentifier) {
+        switch (fragmentIdentifier) {
+            case FRAGMENT_NEWS:
+                this.showNewsFragment();
+                break;
+            case FRAGMENT_PROFILE:
+                this.showProfileFragment();
+                break;
+            case FRAGMENT_SETTINGS:
+                this.showSettingsFragment();
+                break;
+        }
     }
 
     private void configureToolbar() {
@@ -72,5 +98,26 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private void configureNavigationView() {
         this.navigationView = findViewById(R.id.activity_main_nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void showProfileFragment() {
+        if (this.fragmentProfile == null) this.fragmentProfile = ProfileFragment.newInstance();
+            this.startTransactionFragment(this.fragmentProfile);
+        }
+
+    private void showSettingsFragment() {
+        if (this.fragmentSettings == null) this.fragmentSettings = SettingsFragment.newInstance();
+        this.startTransactionFragment(this.fragmentSettings);
+    }
+
+    private void showNewsFragment() {
+        if (this.fragmentNews == null) this.fragmentNews = NewsFragment.newInstance();
+        this.startTransactionFragment(this.fragmentNews);
+    }
+
+    private void startTransactionFragment(Fragment fragment) {
+        if (!fragment.isVisible()){
+            getSupportFragmentManager().beginTransaction().replace(R.id.activity_main_frame_layout, fragment).commit();
+        }
     }
 }
